3304d379f8f6578f62db1c1868538bb5f327439d
Jorge <jym990@gmail.com>
2020-07-18 18:56:17 -0500

    Updates

---
+++ precache-manifest.e0025861d34b3371185d89d49d661dfd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29368e46f7bbda625b59ff30ce906b2e",
+    "url": "/slider-manager/index.html"
+  },
+  {
+    "revision": "669bec117cf6cbee4971",
+    "url": "/slider-manager/static/css/main.ee3b2609.chunk.css"
+  },
+  {
+    "revision": "d549498476bbb9b1f1fb",
+    "url": "/slider-manager/static/js/2.0c98e210.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/slider-manager/static/js/2.0c98e210.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "669bec117cf6cbee4971",
+    "url": "/slider-manager/static/js/main.cf9003f6.chunk.js"
+  },
+  {
+    "revision": "665eb8c3e1f60f366447",
+    "url": "/slider-manager/static/js/runtime-main.e26cbe15.js"
+  }
+]);
